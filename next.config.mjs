44891689fa2bcc6d48a15a956c33a9e7f969642a
Junyu <me@junyu.dev>
2023-08-02 23:50:21 +0800

    build: 修改项目部署方式

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 import WithMDX from "@next/mdx";
 import remarkFrontmatter from "remark-frontmatter";
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import rehypePrettyCode from "rehype-pretty-code";
 
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
-  output: 'export',
 };
 
 /** @type {import('rehype-pretty-code').Options} */
